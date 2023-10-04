6e1e6dee23bc5a96d0f50a678e95a3cd72d52117
dcho <dcho.null@gmail.com>
2023-10-04 09:45:33 -0400

    dz60: add HOLD_ON_OTHER_KEY_PRESS_PER_KEY via

---
+++ keyboards/dz60/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* VIA related config */
+#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
+
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
--- keyboards/dz60/keymaps/via/keymap.c
+++ keyboards/dz60/keymaps/via/keymap.c
@@ -1,5 +1,16 @@
 #include QMK_KEYBOARD_H
 
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1, KC_SPACE):
+            // Do not select the hold action when another key is pressed.
+            return false;
+        default:
+            // Immediately select the hold action when another key is pressed.
+            return true;
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	LAYOUT(
